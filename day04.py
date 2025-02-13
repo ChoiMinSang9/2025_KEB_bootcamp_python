925cb1595993d041dfa05ba9778e30c743e1902b
ChoiMinSang <min393917@gmail.com>
2025-02-13 16:39:43 +0900

    v2.4 day04 assignment

--- day04.py
+++ day04.py
@@ -1,6 +1,14 @@
-# SOLID
-# Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀있는 원칙)
+# Assignment
+# v3.6) 2중 데코레이터 적용. 성능측정 데코레이터, 디스크립션 데코레이터를 팩토리얼 함수에 적용하시오.
 import time
+
+def description_decorator(func):
+    def wrapper(*arg):
+        print(func.__name__)
+        print(func.__doc__)
+        r = func(*arg)
+        return r
+    return wrapper
 
 
 def time_decorator(func):
@@ -13,7 +21,13 @@ def time_decorator(func):
     return wrapper
 
 @time_decorator
+@description_decorator
 def factorial_repetition(n) -> int:
+    """
+    factorial function by loop
+    :param n:
+    :return: results of factorial operation
+    """
     result = 1
     for i in range(2, n+1):
         result = result * i
@@ -21,7 +35,11 @@ def factorial_repetition(n) -> int:
 
 
 number = int(input())
-# s = time.time()
 print(f"{number}! = {factorial_repetition(number)}")
-# e = time.time()
-# print(e-s)
+
+
+# number = int(input())
+# ft = time_decorator(factorial_repetition)
+# print(f"{number}! = {ft(number)}")
+# number = int(input())
+# print(f"{number}! = {factorial_repetition(number)}")
