dd89fa43bcd901f7a6d5f84e5c61ab4e76d2c6c9
ChoiMinSang <min393917@gmail.com>
2025-02-14 11:18:25 +0900

    v3.0 day05 countdown_loop_and_recursion

--- day05.py
+++ day05.py
@@ -1,30 +1,21 @@
-# edit commit
-def fibo_recursion(n) -> int:
-    """
-    fibonacci version recursion
-    :param n: integer
-    :return: result of fibonacci
-    """
-    if n < 0:
-        return 0
-    elif n == 1:
-        return 1
+def countdown_loop(n):
+    for i in range(n,-1,-1):
+        if i ==0:
+            print("bang!")
+        else:
+            print(i)
+
+
+def countdown_recursion(n):
+    if n<0:
+        return
+    if n==0:
+        print("bang~")
     else:
-        return fibo_recursion(n - 2) + fibo_recursion(n - 1)
+        print(n)
+    countdown_recursion(n-1)
 
 
-def fibo_loop(n) -> int:
-    """
-        fibonacci version loop
-        :param n: integer
-        :return: result of fibonacci
-        """
-    n_list=[0,1]
-    for i in range(n+1):
-        n_list.append(n_list[i]+n_list[i+1])
-
-    return n_list[n]
-
-n=int(input())
-print(fibo_loop(n))    #fast speed
-print(fibo_recursion(n))    #low speed
+n = int(input())
+countdown_loop(n)
+countdown_recursion(n)
